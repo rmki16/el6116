ac6221a060dc1cc274e9b9dbeeb52088809b1498
Baha ID <coding@baha.web.id>
2017-09-27 17:32:34 +0700

    Update config.php 1. Replace values

--- tugas3/php-code/config.php
+++ tugas3/php-code/config.php
@@ -6,9 +6,9 @@
  */
 
 $host       = "10.10.20.50";
-$username   = "pesertakelasuser";
-$password   = "pesertakelasuser";
-$dbname     = "pesertakelas";
+$username   = "tugas3";
+$password   = "tugas3";
+$dbname     = "employees";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
