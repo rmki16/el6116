abd7784b4aba78fbfd5bc6532c01350f08858628
Baha ID <coding@baha.web.id>
2017-09-27 22:29:14 +0700

    Init daftar_manajer.php

---
+++ tugas3/php-code/daftar_manajer.php
@@ -0,0 +1,47 @@
+<?php
+
+  try
+  {
+    require "config.php";
+    require "common.php";
+
+    $connection = new PDO($dsn, $username, $password, $options);
+
+    $sql = "SELECT * FROM daftar_manajer";
+
+    $statement = $connection -> prepare($sql);
+    $statement -> execute();
+
+    $result = $statement -> fetchAll();
+
+  } catch (PDOException $error)
+  {
+    echo $sql . "<br>" . $error -> getMessage();
+  }
+
+  <?php
+  if($result && $statement -> rowCount() > 0)
+  { ?>
+    <h2>Daftar Manajer</h2>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Departemen</th>
+          <th>Nama Depan</th>
+          <th>Nama Belakang</th>
+        </tr>
+      </thead>
+      <tbody>
+    <?php
+      foreach ($resutl as $row)
+      { ?>
+        <tr>
+          <td><?php echo escape ($row["Departemen"]); ?></td>
+          <td><?php echo escape ($row["Nama Depan"]); ?></td>
+          <td><?php echo escape ($row["Nama Belakang"]); ?></td>
+        </tr>
+      <?php
+      } ?>
+  }
+   ?>
