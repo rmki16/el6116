7045cdfe532677ac6cf30af8d618adfde0bcbe5a
Baha ID <coding@baha.web.id>
2017-09-27 20:33:44 +0700

    Init header.php

---
+++ tugas3/php-code/templates/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>EL6116 | Tugas 3 Inference dan Statistical Attack</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>EL6116 | Tugas 3 Inference dan Statistical Attack</h1>
