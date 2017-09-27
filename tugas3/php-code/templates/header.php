2cd78db2bea5dfa8eb5b6c8faad8f1e1ffa45068
Baha ID <coding@baha.web.id>
2017-09-27 21:30:39 +0700

    Update header.php 1. Change the title

--- tugas3/php-code/templates/header.php
+++ tugas3/php-code/templates/header.php
@@ -6,10 +6,10 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>EL6116 | Tugas 3 Inference dan Statistical Attack</title>
+	<title>EL6116: Tugas 3</title>
 
 	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-	<h1>EL6116 | Tugas 3 Inference dan Statistical Attack</h1>
+	<h1>EL6116: Tugas 3</h1>
